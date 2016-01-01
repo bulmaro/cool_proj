cb055f20a2656dae7c6c6cddaf17dc46aeb873dd
esteban herrera <estebanherrera2@gmail.com>
2016-01-01 00:26:56 +0000

    thisoneworks.py has my name animated into it. Next step is animate motion

--- pi/animateNameEsteban.py
+++ pi/animateNameEsteban.py
@@ -12,28 +12,26 @@ for k in out.keys():
   GPIO.setup(out[k], GPIO.OUT)
 
 # for cathodes/columns set to 0 to turn on
-def set_cathodes(byte):
-# GPIO.output(out["c0"], GPIO.HIGH if byte & 0x01 == 0x01 else GPIO.LOW ) 
-  GPIO.output(out["c0"], 0 if byte & 0x128 == 0x128 else 1) # col 0 
-  GPIO.output(out["c1"], 0 if byte & 0x064 == 0x064 else 1) # col 1
-  GPIO.output(out["c2"], 0 if byte & 0x032 == 0x032 else 1) # col 2
-  GPIO.output(out["c3"], 0 if byte & 0x016 == 0x016 else 1) # col 3
-  GPIO.output(out["c4"], 0 if byte & 0x008 == 0x008 else 1) # col 4
-  GPIO.output(out["c5"], 0 if byte & 0x004 == 0x004 else 1) # col 5
-  GPIO.output(out["c6"], 0 if byte & 0x002 == 0x002 else 1) # col 6
-  GPIO.output(out["c7"], 0 if byte & 0x001 == 0x001 else 1) # col 7
+def set_cathodes(col):
+  GPIO.output(out["c0"], 0 if col == 0 else 1) # col 0 
+  GPIO.output(out["c1"], 0 if col == 1 else 1) # col 1
+  GPIO.output(out["c2"], 0 if col == 2 else 1) # col 2
+  GPIO.output(out["c3"], 0 if col == 3 else 1) # col 3
+  GPIO.output(out["c4"], 0 if col == 4 else 1) # col 4
+  GPIO.output(out["c5"], 0 if col == 5 else 1) # col 5
+  GPIO.output(out["c6"], 0 if col == 6 else 1) # col 6
+  GPIO.output(out["c7"], 0 if col == 7 else 1) # col 7
 
 # for anodes/rows set to 1 to turn on
 def set_anodes(row):
-# GPIO.output(out["a0"], GPIO.HIGH if byte & 0x01 == 0x01 else GPIO.LOW ) 
   GPIO.output(out["a0"], 1 if row == 0 else 0) # row 0
-  GPIO.output(out["a1"], 1 if byte & 0x002 == 0x002 else 0) # row 1
-  GPIO.output(out["a2"], 1 if byte & 0x004 == 0x004 else 0) # row 2
-  GPIO.output(out["a3"], 1 if byte & 0x008 == 0x008 else 0) # row 3
-  GPIO.output(out["a4"], 1 if byte & 0x016 == 0x016 else 0) # row 4
-  GPIO.output(out["a5"], 1 if byte & 0x032 == 0x032 else 0) # row 5
-  GPIO.output(out["a6"], 1 if byte & 0x064 == 0x064 else 0) # row 6
-  GPIO.output(out["a7"], 1 if byte & 0x128 == 0x128 else 0) # row 7
+  GPIO.output(out["a1"], 1 if row == 1 else 0) # row 1
+  GPIO.output(out["a2"], 1 if row == 2 else 0) # row 2
+  GPIO.output(out["a3"], 1 if row == 3 else 0) # row 3
+  GPIO.output(out["a4"], 1 if row == 4 else 0) # row 4
+  GPIO.output(out["a5"], 1 if row == 5 else 0) # row 5
+  GPIO.output(out["a6"], 1 if row == 6 else 0) # row 6
+  GPIO.output(out["a7"], 1 if row == 7 else 0) # row 7
 
 movie = [
     [0b11111000,0b10000000,0b1000000,0b11100000,0b10000000,0b10000000,0b10000000,0b11111000], # E
@@ -53,3 +51,4 @@ def showAnimation(movie):
     showBitmap(bitmap, 1)
 
 showAnimation(movie)
+
--- pi/tryall.py
+++ pi/tryall.py
@@ -14,26 +14,26 @@ for k in out.keys():
 # for cathodes/columns set to 0 to turn on
 def set_cathodes(byte):
 # GPIO.output(out["c0"], GPIO.HIGH if byte & 0x01 == 0x01 else GPIO.LOW ) 
-  GPIO.output(out["c0"], 0 if byte & 0x128 == 0x128 else 1) # col 0 
-  GPIO.output(out["c1"], 0 if byte & 0x064 == 0x064 else 1) # col 1
-  GPIO.output(out["c2"], 0 if byte & 0x032 == 0x032 else 1) # col 2
-  GPIO.output(out["c3"], 0 if byte & 0x016 == 0x016 else 1) # col 3
-  GPIO.output(out["c4"], 0 if byte & 0x008 == 0x008 else 1) # col 4
-  GPIO.output(out["c5"], 0 if byte & 0x004 == 0x004 else 1) # col 5
-  GPIO.output(out["c6"], 0 if byte & 0x002 == 0x002 else 1) # col 6
-  GPIO.output(out["c7"], 0 if byte & 0x001 == 0x001 else 1) # col 7
+  GPIO.output(out["c0"], 0 if byte & 128 == 128 else 1) # col 0 
+  GPIO.output(out["c1"], 0 if byte & 064 == 064 else 1) # col 1
+  GPIO.output(out["c2"], 0 if byte & 032 == 032 else 1) # col 2
+  GPIO.output(out["c3"], 0 if byte & 016 == 016 else 1) # col 3
+  GPIO.output(out["c4"], 0 if byte & 008 == 008 else 1) # col 4
+  GPIO.output(out["c5"], 0 if byte & 004 == 004 else 1) # col 5
+  GPIO.output(out["c6"], 0 if byte & 002 == 002 else 1) # col 6
+  GPIO.output(out["c7"], 0 if byte & 001 == 001 else 1) # col 7
 
 # for anodes/rows set to 1 to turn on
 def set_anodes():
 # GPIO.output(out["a0"], GPIO.HIGH if byte & 0x01 == 0x01 else GPIO.LOW ) 
-  GPIO.output(out["a0"], 1 if byte & 0x001 == 0x001 else 0) # row 0
-  GPIO.output(out["a1"], 1 if byte & 0x002 == 0x002 else 0) # row 1
-  GPIO.output(out["a2"], 1 if byte & 0x004 == 0x004 else 0) # row 2
-  GPIO.output(out["a3"], 1 if byte & 0x008 == 0x008 else 0) # row 3
-  GPIO.output(out["a4"], 1 if byte & 0x016 == 0x016 else 0) # row 4
-  GPIO.output(out["a5"], 1 if byte & 0x032 == 0x032 else 0) # row 5
-  GPIO.output(out["a6"], 1 if byte & 0x064 == 0x064 else 0) # row 6
-  GPIO.output(out["a7"], 1 if byte & 0x128 == 0x128 else 0) # row 7
+  GPIO.output(out["a0"], 1 if byte & 001 == 001 else 0) # row 0
+  GPIO.output(out["a1"], 1 if byte & 002 == 002 else 0) # row 1
+  GPIO.output(out["a2"], 1 if byte & 004 == 004 else 0) # row 2
+  GPIO.output(out["a3"], 1 if byte & 008 == 008 else 0) # row 3
+  GPIO.output(out["a4"], 1 if byte & 016 == 016 else 0) # row 4
+  GPIO.output(out["a5"], 1 if byte & 032 == 032 else 0) # row 5
+  GPIO.output(out["a6"], 1 if byte & 064 == 064 else 0) # row 6
+  GPIO.output(out["a7"], 1 if byte & 128 == 128 else 0) # row 7
 
 def showBitmap(bitmap, showFor)
   frames_per_second = 50
