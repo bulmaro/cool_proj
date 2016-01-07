c31c6562ebb842a217cb311c02c8e30d2e80cc8e
Esteban Herrera <esteban.herrera@carpoolagency.com>
2016-01-07 15:28:26 -0800

    rough draft untested scrolling function, based on the showBitmap question

--- pi/scrollingLetters/scroller.py
+++ pi/scrollingLetters/scroller.py
@@ -7,18 +7,28 @@ def scrollString(string):
   string = string.lower()
   index = 0
 
-  display_buffer = [0,0,0,0,0,0,0,0]
-  letter = string[index]
-  letter_bitmap = alpha[letter]
-  print letter_bitmap
-
+  #iterates through the string one char at a time
   while True:
     print string[index]
-
-
-
+    letter = string[index]
+    letter_bitmap = alpha[letter]
+    for shift in range(0,8):
+      frames_per_second = 50
+      for n in range(0, int(showFor*frames_per_second)):
+        for row in range(0,len(letter_bitmap)):
+          set_anodes(1<<row)
+          set_cathodes( letter_bitmap[row]<<shift )
+          time.sleep(1.0/(len(bitmap)*frames_per_second))
     index+=1
     if index >= len(string):
       break
+
+def showBitmap(bitmap, showFor):
+    frames_per_second = 50
+    for n in range(0, int(showFor*frames_per_second)):
+      for row in range(0,len(bitmap)):
+        set_anodes( 1<<row )
+        set_cathodes( bitmap[row] )
+        time.sleep(1.0/(len(bitmap)*frames_per_second))
 
 scrollString("Esteban")
