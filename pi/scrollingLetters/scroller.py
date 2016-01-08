c82debbc8f11db490d334608b0f57df26395f909
esteban herrera <estebanherrera2@gmail.com>
2016-01-08 04:29:02 +0000

    single letters are scrolling one at a time

--- pi/scrollingLetters/scroller.py
+++ pi/scrollingLetters/scroller.py
@@ -1,7 +1,7 @@
 from LED_Matrix_Display import *
 from alphabet import *
 
-# Display.setup()
+Display().setup()
 
 def scrollString(string):
   string = string.lower()
@@ -14,12 +14,12 @@ def scrollString(string):
 
     for shift in range(0,8):
       frames_per_second = 50
-      showFor = 1
+      showFor = .1
       for n in range(0, int(showFor*frames_per_second)):
         for row in range(0,len(letter_bitmap)):
           Display().set_anodes(1<<row)
           Display().set_cathodes( letter_bitmap[row]<<shift )
-          time.sleep(1.0/(len(bitmap)*frames_per_second))
+          time.sleep(1.0/(len(letter_bitmap)*frames_per_second))
 
     index+=1
     if index >= len(string):
